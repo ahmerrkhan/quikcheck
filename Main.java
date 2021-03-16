0edbad3367b5310d4fa22c1e5d8da4e330e07d64
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-03-16 19:32:20 +0500

    taking inputs

--- Main.java
+++ Main.java
@@ -1,70 +1,22 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	//Data Types
-        int a1 = 12;
-        float a2 = 23.4f;
-        double a3 = 34.444;
-        byte a4 = 3;
-        System.out.println("value of a1 : "+a1);
-        System.out.println("value of a2 : "+a2);
-        System.out.println("value of a3 : "+a3);
-        System.out.println("value of a4 : "+a4);
-        String str1 = "Muhammad Ahmer khan";
-        System.out.println("Name is : "+str1);
-        char charID = 'X';
-        System.out.println("Character is : "+charID);
-        System.out.println("------------------------");
+        //..program to take input from user and print on screen
+          Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a  integer : ");
+        int a=sc.nextInt();
+        System.out.println("You entered integer: "+a);
+        System.out.println("Enter a float : ");
+        float b = sc.nextFloat();
+        System.out.println("You entered float : "+b);
+        System.out.println("Enter your name in String : ");
+        String str = sc.nextLine();
+        System.out.println("You entered : "+str);
 
-        //Operators in Java
-        int b1,b2,b3,b4;
-        b1=10;
-        b2=5;
-        System.out.println("Addition is : "+(b1+b2));
-        System.out.println("Subtraction is : "+(b1-b2));
-        System.out.println("Multiplication is :"+(b1*b2));
-        System.out.println("Division is : "+(b1/b2));
-        b3=2;
-        System.out.println("postfix increment : "+(b3++));
-        System.out.println("lets check it again : "+(b3));
-        b4=1;
-        System.out.println("prefic increment : "+(++b4));
-        System.out.println("Lets check it again : "+(b4));
-        System.out.println("----------------------");
-        //Control Flows statements in java
 
-        //If-Else statement
-        int c1 = 10;
-        if(c1>11){
-            System.out.println("True");
-        }else {
-            System.out.println("false");
-        }
-        System.out.println("-----------------");
-
-        //For loop
-        int c2  =1;
-        for(c2=1;c2<10;c2++){
-            System.out.println(c2);
-        }
-        System.out.println("---------------");
-
-        //While Loop
-        int c3=1;
-        while(c3<20){
-            System.out.println(c3);
-            c3++;
-        }
-        System.out.println("---------------");
-
-        //Do While loop
-        int c4=2;
-        do{
-            System.out.println(c4);
-            c4++;
-        }while(c4<15);
 
 
 
